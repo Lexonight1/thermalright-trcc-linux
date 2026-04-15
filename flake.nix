37896e3cb5d6a86136acb10372c8e339bb5c7287
skylar <skylar@elsie.cafe>
2026-04-15 23:40:09 +1200

    fix: use stable for pyside6 pkg

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "TRCC Linux — Thermalright LCD/LED Control Center";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -33,6 +33,7 @@
             python-multipart
             prompt-toolkit
             sounddevice
+            certifi
           ];
 
           optional-dependencies = {
